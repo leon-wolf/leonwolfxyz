b86871e9553c21b24f9508e7d24f33fb864f388d
Leon Wolf <leon.wolf@sectornord.de>
2020-05-03 23:39:15 +0200

    Changed Excerpt to Subtitle

--- components/blogPost.vue
+++ components/blogPost.vue
@@ -1,39 +1,36 @@
 <template>
-    <v-card hover flat width="200px" class="blogPost">
-        <v-card-title>
-            {{ article.title }}
-        </v-card-title>
-        <v-card-subtitle>
-            {{ article.created_at }}
-        </v-card-subtitle>
-        <v-card-text v-if="article"> {{excerpt}}</v-card-text>
-        <v-card-text v-else>FOO</v-card-text>
-        <v-card-actions>
-            <nuxt-link :to="'/blog/post/' + article.slug">
-                <v-btn
-                        text
-                        color="deep-grey accent-4"
-                >
-                    Read More
-                </v-btn>
-            </nuxt-link>
+  <v-card hover flat width="200px" class="blogPost">
+    <v-card-title>
+      {{ article.title }}
+    </v-card-title>
+    <v-card-subtitle>
+      {{ article.created_at }}
+    </v-card-subtitle>
+    <v-card-text v-if="article"> {{ excerpt }}</v-card-text>
+    <v-card-text v-else>FOO</v-card-text>
+    <v-card-actions>
+      <nuxt-link :to="'/blog/post/' + article.slug">
+        <v-btn text color="deep-grey accent-4">
+          Read More
+        </v-btn>
+      </nuxt-link>
     </v-card-actions>
-    </v-card>
+  </v-card>
 </template>
 
 <script>
-    export default {
-        name: 'blogPost',
-        computed: {
-            excerpt: function () {
-                return this.article.body.substring(0, 111) + '...'
-            }
-        },
-        props: ['article', 'users']
+export default {
+  name: 'blogPost',
+  computed: {
+    excerpt: function() {
+      return this.article.subtitle
     }
+  },
+  props: ['article', 'users']
+}
 </script>
 <style>
-    .blogPost {
-        margin: 10px;
-    }
+.blogPost {
+  margin: 10px;
+}
 </style>
